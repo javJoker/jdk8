00d6e12ed1287ea2dce4300250864690df632662
pengdeyi <pengdeyi@chinayasha.com>
2019-08-07 20:18:10 +0800

    serializable

--- src/main/java/java/io/Serializable.java
+++ src/main/java/java/io/Serializable.java
@@ -166,5 +166,48 @@ package java.io;
  * @see java.io.Externalizable
  * @since   JDK1.1
  */
+
+/**
+ * 实现java.io.Serializable接口的类启用了类的可序列化。
+ * 未实现此接口的类将不会将其任何状态序列化或反序列化。
+ * 可序列化类的所有子类型本身都是可序列化的。
+ * 序列化接口没有方法或字段，仅用于标识可序列化的语义。
+ *
+ * 为了允许非序列化类的子类型被序列化，
+ * 子类型可以承担保存和恢复超类型的公共、受保护和（可访问）包字段的状态的责任。
+ * 只有当它扩展的类具有可访问的无参构造函数来初始化类的状态时，子类型才可以承担此责任。
+ * 如果不是这种情况，则声明类Serializable是错误的。将在运行时检测到错误。
+ *
+ * 在反序列化期间，通过使用类的public或protected 无参构造函数初始化非序列化类的字段。
+ * 对可序列化的子类访问无参构造函数。序列化子类的字段将从流中恢复。
+ *
+ * 遍历图形时，可能会遇到不支持Serializable接口的对象。
+ * 在这种情况下，将抛出NotSerializableException，并将标识非可序列化对象的类。
+ *
+ * 在序列化和反序列化过程中需要特殊处理的类必须使用这些精确签名实现特殊方法：
+ * （1）private void writeObject(java.io.ObjectOutputStream out) throws IOException
+ * （2）private void readObject(java.io.ObjectInputStream in) throws IOException, ClassNotFoundException;
+ * （3）private void readObjectNoData() throws ObjectStreamException;
+ *
+ * 1）writeObject方法负责为其特定类编写对象的状态，以便相应的readObject方法可以恢复它。
+ * 可以通过调用out.defaultWriteObject来调用保存Object字段的默认机制。
+ * 该方法不需要关注属于其超类或子类的状态。
+ * 通过使用writeObject方法或使用DataOutput支持的原始数据类型的方法将各个字段写入ObjectOutputStream来保存状态。
+ *
+ * 2）readObject方法负责从流中读取并恢复类字段。
+ * 它可以调用in.defaultReadObject来调用恢复对象的非静态和非瞬态字段的默认机制。
+ * defaultReadObject方法使用流中的信息来指定流中保存的对象的字段以及当前对象中相应命名的字段。
+ * 这处理了类在演变为添加新字段时的情况。该方法不需要关注属于其超类或子类的状态。
+ * 通过使用writeObject方法或使用DataOutput支持的原始数据类型的方法将各个字段写入ObjectOutputStream来保存状态。
+ *
+ * 3）
+ * readObjectNoData方法负责在序列化流未将给定类列为要反序列化的对象的超类的情况下初始化其特定类的对象的状态。
+ * 如果接收方使用与发送方不同版本的反序列化实例的类，并且接收方的版本扩展了未由发送方版本扩展的类，
+ * 则可能发生这种情况。如果序列化流已被篡改，也可能发生这种情况;因此，尽管存在“恶意”或不完整的源流，
+ * readObjectNoData仍可用于正确初始化反序列化对象。
+ *
+ * 127
+ *
+ */
 public interface Serializable {
 }
